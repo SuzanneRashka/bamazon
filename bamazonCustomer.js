04539574271c44337b9ca4136c43cb7b0d413aad
SuzanneRashka <suzanne.rashka@gmail.com>
2018-08-27 19:18:56 -0500

    printing list

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,6 +1,6 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
-var Table = require("cli-table3");
+const cTable = require('console.table');
 
 var connection = mysql.createConnection({
   host: "localhost",
@@ -10,7 +10,7 @@ var connection = mysql.createConnection({
   database: "bamazonDB"
 });
 
-connection.connect(function(err) {
+connection.connect(function (err) {
   if (err) throw err;
   console.log("connected as id " + connection.threadId + "\n");
   displayAllItems();
@@ -18,47 +18,49 @@ connection.connect(function(err) {
 
 function displayAllItems() {
   //   console.log("Selecting all products... \n");
-  connection.query("SELECT * FROM products", function(err, res) {
+  connection.query("SELECT * FROM products", function (err, res) {
     if (err) throw err;
     inquirer
-      .prompt([
-        {
-          name: "initialize",
-          type: "rawlist",
-          choices: function() {
-            var choiceArray = [];
-            for (var i = 0; i < results.length; i++) {
-              choiceArray.push(results[i].product_name);
-            }
-            return choiceArray;
-          },
-          message: "What item would you like to purchase?"
-        }
-      ])
-      .then(function(anser) {
-        var chosenItem;
-        for (var i = 0; i < results.length; i++) {
-          if (results[i].product_name === answer.choice) {
-            chosenItem = results[i];
+      .prompt([{
+        name: "initialize",
+        type: "input",
+        choices: function () {
+          var choiceArray = [];
+          var tableArray = [];
+          for (var i = 0; i < res.length; i++) {
+            tableArray.push(res[i]);
           }
-        }
+          console.table(tableArray);
+          return choiceArray;
+        },
+        message: "What is the item number of the product you would like to buy?"
+      }])
+      .then(function (answer) {
+        quantity(answer);
       });
   });
 }
 
+function quantity(answer) {
+  inquirer.prompt([{
+    name: "quantity",
+    type: "input",
+    message: "How many would you like to purchase?"
+  }]).then(function (number) {
+    if (answer.quantity) {
+      minus stock--
+      if (stock < 0) {
+        console.log("We dont have enough product for you")
+      }
+
+    }
+  })
+}
+
+function hasStock(answer) {
+  console.log(answer.stock_quantity + "quantity")
+}
 // product_name, department_name, price, stock_quantity
 
-function printsToTable() {
-  var table = new Table({
-    head: ["ID", "Product", "Department", "Price", "Quantity"],
-    colWidths: [10, 40, 20, 10, 10]
-  });
-
-  // table is an Array, so you can `push`, `unshift`, `splice` and friends
-  table.push(
-    ["1", "Item one", "kitchen", "$", 20],
-    ["2", "Item two", "electronics", "$", 10]
-  );
-
-  console.log(table.toString());
-} // end function
+//   console.log(table.toString());
+// } // end function
